88dcc9a4543ebd7f3ab07a05c3d37ac0042f762f
johannymk7 <johannymk7@github.com>
2023-11-11 21:52:44 +0100

    Añadir ejercicio-10

--- src/ejercicio-10/index.js
+++ src/ejercicio-10/index.js
@@ -1,1 +1,24 @@
 // Escribe tu código aquí:
+
+// Definir la clase Person primero
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+    constructor(name, age, grade) {
+        super(name, age)
+        this.grade = grade
+    }
+
+    introduce() {
+        console.log(`Hola, soy, ${this.name}, tengo ${this.age} años y estoy en el grado ${this.grade},`)
+    }
+}
+
+const student = new Student('Bob',19, 12)
+
+student.introduce()
