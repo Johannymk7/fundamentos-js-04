a12fd435c5c24a5555c3b463c914c91c9c061133
johannymk7 <johannymk7@github.com>
2023-11-11 20:39:04 +0100

    Añadir ejercicio-06

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -1,1 +1,24 @@
 // Escribe tu código aquí:
+class Rectangle {
+    constructor(length, width) {
+      this.length = length;
+      this.width = width;
+    }
+  
+    calculateArea() {
+      return this.length * this.width;
+    }
+  }
+  
+  class Square extends Rectangle {
+    constructor(sidelength) {
+      // Llama al constructor de la clase padre (Rectangle)
+      super(sidelength, sidelength);
+    }
+  }
+  
+  // Crea una instancia de la clase Square
+  const square = new Square(7);
+  
+  // Accede al método calculateArea de la clase Rectangle (que es la clase padre de Square)
+  console.log('Área del cuadrado:', square.calculateArea());
